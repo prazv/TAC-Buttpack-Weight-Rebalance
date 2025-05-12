9407736b5a9edd2fac98413a93fb2e6387998d70
prazv <64920464+prazv@users.noreply.github.com>
2025-05-12 20:27:16 +0300

    Add files via upload

---
+++ Addons/config.cpp
@@ -0,0 +1,281 @@
+class CfgPatches
+{
+	class Buttpack_Rebalance
+	{
+		version="1.3";
+		units[]={};
+		weapons[]={};
+		requiredVersion="1.1.3";
+		requiredAddons[]={};
+	};
+};
+class CfgVehicles
+{
+    class Bag_Base;
+    class TAC_MTAP_RG: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_BK: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_CY: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_MTP: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_RG2: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_BK2: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_CY2: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_MTAP_MTP2: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP_B: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP_Khaki: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP2_B: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP2_Khaki: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP2_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP3_B: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP3_Khaki: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_HSG_GLP3_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BAL_BK: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BAL_KH: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BAL_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_ACU: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_B: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_Khaki: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_MTP: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_RG: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2ACU: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_B2: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_Khaki: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_MTP: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_OD: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2RG: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2ACUM: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt_B2M: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_KhakiM: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_MTPM: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2_ODM: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+    class TAC_BP_Butt2RGM: Bag_Base
+    {
+        class ItemInfo
+        {
+            mass = 10;
+        };
+    };
+};
---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "TAC Buttpack Rebalance";
+timestamp = 5250512691603158694;
